9aae2a31bd2bc410a3834818677c1bad2e88a243
Kichirooo <andrew.reynard.h@gmail.com>
2026-02-13 22:53:13 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000110585_AndrewReynardHamdani/week02/Student.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week02/Student.kt
@@ -1,6 +1,11 @@
 package oop_00000110585_AndrewReynardHamdani.week02
 
-class Student (val name: String, val nim: String, var major: String) {
+class Student (
+    val name: String
+    val nim: String
+    val gpa: Double = 0.0
+    var major: String
+) {
     init {
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
