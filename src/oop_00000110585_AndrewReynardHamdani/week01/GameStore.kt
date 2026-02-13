48c3c18a699e9237feb7c8ad3fd35c9ec65f561e
Kichirooo <andrew.reynard.h@gmail.com>
2026-02-13 22:00:01 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000110585_AndrewReynardHamdani/week01/GameStore.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week01/GameStore.kt
@@ -1,5 +1,12 @@
 package oop_00000110585_AndrewReynardHamdani.week01
 
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) {
+    (price * 0.2).toInt()
+} else {
+    (price * 0.1).toInt()
+}
+
 fun main() {
     val gameTitle = "TEKKEN 8"
     val price = 700000
