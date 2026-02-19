cf5bb539e4f55521af0bd6cff878fd347aa7747c
Kichirooo <andrew.reynard.h@gmail.com>
2026-02-19 22:15:14 +0700

    week 02 (task 1) create Loan classwith default args

---
+++ src/oop_00000110585_AndrewReynardHamdani/week02/Loan.kt
@@ -0,0 +1,34 @@
+package oop_00000110585_AndrewReynardHamdani.week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1
+) {
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
+
+fun printReceipt(title: String, finalPrice: Int, userNote: String? = null) {
+    val note = userNote ?: "Tidak ada catatan"
+    println("Judul: $title")
+    println("Harga Akhir: Rp $finalPrice")
+    println("Catatan: $note")
+}
+
+fun main() {
+    val gameTitle = "Mario Bros"
+    val price = 550000
+    val note: String? = null
+
+    printReceipt(
+        title = gameTitle,
+        finalPrice = price,
+        userNote = note
+    )
+}
