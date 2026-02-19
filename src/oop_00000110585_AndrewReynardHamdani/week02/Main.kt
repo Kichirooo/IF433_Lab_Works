319711a6d4d024aa6e168d3e1a47c6dc655a4c13
Kichirooo <andrew.reynard.h@gmail.com>
2026-02-19 22:04:37 +0700

    week02: implement selection logic for constructors

--- src/oop_00000110585_AndrewReynardHamdani/week02/Main.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week02/Main.kt
@@ -3,10 +3,10 @@ package oop_00000110585_AndrewReynardHamdani.week02
 import java.util.Scanner
 
 class Student (
-    val name: String
-    val nim: String
-    val gpa: Double = 0.0
-    var major: String
+    val name: String,
+    val nim: String,
+    val gpa: Double = 0.0,
+    var major: String,
 ) {
     init {
         if (nim.length != 5) {
@@ -17,7 +17,7 @@ class Student (
         }
     }
 
-    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+    constructor(name: String, nim: String) : this(name, nim, 0.0, "Non-Matriculated") {
         println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
 }
@@ -32,32 +32,30 @@ fun main() {
 
     print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
-
     scanner.nextLine()
 
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
     } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
+        println("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
 
-        val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai.")
-    }
+        if (type == 1) {
+            print("Masukkan Jurusan: ")
+            val majorInput = scanner.nextLine()
 
-    print("Pilih Jalur (1. Reguler, 2. Umum: ")
-    val type = scanner.nextInt()
-    scanner.nextLine()
+            val s1 = Student(name = name, nim = nim, major = majorInput)
+            println("Status: Pendaftaran Selesai.")
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
 
-    if (type == 1) {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
-        val s1 = Student(name, nim, major)
-        println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa} ")
-    } else if (type == 2) {
-        val s2 = Student(name, nim)
-        println("Terdaftar di ${s2.major} dengan GPA awal ${s2.gpa} ")
-    } else {
-        println("Pilihan ngawur, pendaftaran batal!")
+        } else if (type == 2) {
+            val s2 = Student(name, nim)
+            println("Status: Pendaftaran Selesai.")
+            println("Terdaftar di ${s2.major} dengan GPA awal ${s2.gpa}")
+
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
     }
 }
