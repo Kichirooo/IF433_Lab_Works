0e31f68d4f4655bfaf350d988f261bb9ecb3150d
Kichirooo <andrew.reynard.h@gmail.com>
2026-02-20 11:40:26 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000110585_AndrewReynardHamdani/week03/Employee.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week03/Employee.kt
@@ -2,5 +2,9 @@ package oop_00000110585_AndrewReynardHamdani.week03
 
 class Employee (val name: String) {
     val salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value
+        }
 }
 
