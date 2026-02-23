0d825d511e6b82127c2f3633e413a7c83436b906
Kichirooo <andrew.reynard.h@gmail.com>
2026-02-23 21:42:27 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000110585_AndrewReynardHamdani/week03/Employee.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week03/Employee.kt
@@ -1,24 +1,27 @@
 package oop_00000110585_AndrewReynardHamdani.week03
 
-private var performanceRating: Int = 3
-
-class Employee (val name: String) {
+class Employee(val name: String) {
     var salary: Int = 0
-        set (value) {
-            if(value < 0) {
+        set(value) {
+            if (value < 0) {
                 println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
                 field = 0
             } else {
                 field = value
             }
         }
-}
 
-fun increasePerformance() {
-    performanceRating++
-    println("Kinerja $name meningkat! Rating: $performaceRating")
-}
+    val tax: Double
+        get() = salary * 0.1
 
-fun printStatus() {
-    println("Karyawan: $name, Rating: $performanceRating")
+    private var performanceRating: Int = 3
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
 }
