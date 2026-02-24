0543f9f7c1c4515d5c425594c827fc9b3f6ceabe
Kichirooo <andrew.reynard.h@gmail.com>
2026-02-24 21:59:55 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_00000110585_AndrewReynardHamdani/week03/Main.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week03/Main.kt
@@ -7,8 +7,19 @@ fun main() {
     myWeapon.damage = -50
     println("Damage sekarang: ${myWeapon.damage}")
 
-    myWeapon.damage = 9998
+    myWeapon.damage = 9999
     println("Damage setelah paksa: ${myWeapon.damage}")
 
     println("Tier Senjata: ${myWeapon.tier}")
+
+    println("\n" + "=".repeat(30) + "\n")
+
+    val player = Player("Kichirooo")
+    println("Menambah 50 XP...")
+    player.addXp(50)
+    println("Status saat ini: ${player.username} | Level: ${player.level}")
+
+    println("Menambah 60 XP...")
+    player.addXp(60)
+    println("Status akhir: ${player.username} | Level: ${player.level}")
 }
