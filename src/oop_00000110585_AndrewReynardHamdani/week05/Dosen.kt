3947925acdbcd50e519a97ec98e407f21a7ab836
Kichirooo <andrew.reynard.h@gmail.com>
2026-03-10 22:17:22 +0700

    week05: create abstract class Pegawai

--- src/oop_00000110585_AndrewReynardHamdani/week05/Dosen.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week05/Dosen.kt
@@ -1,6 +1,6 @@
 package oop_00000110585_AndrewReynardHamdani.week05
 
-class Dosen(nama: String, val nidn: String) : Pegawai(nama) {
+class Dosen(nama: String, val nidn: String) : `Pegawai.kt`(nama) {
     override fun bekerja() {
         pritnln("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
     }
