49eba62173cadedebe2c859b695d1fe7f7e40f59
Kichirooo <andrew.reynard.h@gmail.com>
2026-03-13 12:51:04 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000110585_AndrewReynardHamdani/week06/Main.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week06/Main.kt
@@ -1,7 +1,16 @@
 package oop_00000110585_AndrewReynardHamdani.week06
 
 fun main() {
+    val hub = SmartHomeHub()
+
     val lamp = SmartLamp("L01", "Ruang Tamu")
     val speaker = SmartSpeaker("S01", "Google Nest Dapur")
     val cctv = SmartCCTV("C01", "Ezviz Garasi")
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }
