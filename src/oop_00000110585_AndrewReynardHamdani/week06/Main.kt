bc32827e26aec0e57e9446b4278f889beaa329a3
Kichirooo <andrew.reynard.h@gmail.com>
2026-03-13 12:46:30 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_00000110585_AndrewReynardHamdani/week06/Main.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week06/Main.kt
@@ -1,19 +1,7 @@
 package oop_00000110585_AndrewReynardHamdani.week06
 
 fun main() {
-    val hub = SmartHomeHub()
-
     val lamp = SmartLamp("L01", "Ruang Tamu")
     val speaker = SmartSpeaker("S01", "Google Nest Dapur")
     val cctv = SmartCCTV("C01", "Ezviz Garasi")
-
-    hub.addDevice(lamp)
-    hub.addDevice(speaker)
-    hub.addDevice(cctv)
-
-    println("--- Menjalankan Sistem Smart Home ---")
-    hub.activateSecurityMode()
-
-    println("\n--- Mematikan Seluruh Saklar ---")
-    hub.turnOffAllSwitches()
 }
