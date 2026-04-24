e9433d8b5c04a665d24e363043e5a5694007d46b
Kichirooo <andrew.reynard.h@gmail.com>
2026-04-24 22:48:37 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000110585_AndrewReynardHamdani/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000110585_AndrewReynardHamdani.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 10, 15.5, "CLOSED"), // Profit
+        TradeLog("ETHUSDT", "SHORT", 5, -5.2, "CLOSED"), // Loss
+        TradeLog("BTCUSDT", "LONG", 20, -12.0, "OPEN"),  // Loss
+        TradeLog("SOLUSDT", "LONG", 15, 25.0, "CLOSED"), // Profit
+        TradeLog("ETHUSDT", "LONG", 10, 8.5, "OPEN"),    // Profit
+        TradeLog("BTCUSDT", "SHORT", 5, 2.1, "CLOSED")   // Profit
+    )
+}
