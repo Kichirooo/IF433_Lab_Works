cdf9ce973f394f50b2f3e08ff8480d9e75564d3b
Kichirooo <andrew.reynard.h@gmail.com>
2026-04-24 15:18:57 +0700

    week09: implement set to discard duplicate elements

--- src/oop_00000110585_AndrewReynardHamdani/week09/CollectionsBasics.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week09/CollectionsBasics.kt
@@ -18,5 +18,5 @@ fun main() {
     val activeUsers = mutableSetOf("User A", "User B")
     activeUsers.add("UserC")
     activeUsers.add("UserA")
-    println("Mutable List: $scores")
+    println("Active Users: $activeUsers")
 }
