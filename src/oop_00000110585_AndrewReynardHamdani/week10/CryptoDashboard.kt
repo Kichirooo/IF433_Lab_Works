6d52bbda104bf14a38a9783396998dedb689f417
Kichirooo <andrew.reynard.h@gmail.com>
2026-05-14 20:24:46 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000110585_AndrewReynardHamdani/week10/CryptoDashboard.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week10/CryptoDashboard.kt
@@ -13,4 +13,8 @@ fun main() {
     response.data.forEach {
         println(it)
     }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX001", 1.5))
+    txRepo.add(Transaction("TX002", 50.0))
 }
