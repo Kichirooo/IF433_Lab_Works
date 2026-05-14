add381bbc844b595eac74b00369dbdf0892a5ad2
Kichirooo <andrew.reynard.h@gmail.com>
2026-05-14 20:40:20 +0700

    week11: create nullable receiver extension

--- src/oop_00000110585_AndrewReynardHamdani/week11/StringExtensions.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week11/StringExtensions.kt
@@ -1,10 +1,13 @@
 package oop_00000110585_AndrewReynardHamdani.week11
 
-
 fun String.addGreeting(): String {
     return "Hello, $this"
 }
 
 fun String.repeatTimes(n: Int): String {
     return this.repeat(n)
+}
+
+fun String?.isNullOrEmptyCustom(): Boolean {
+    return this == null || this.isEmpty()
 }
