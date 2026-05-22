e57db598512b1a0be1e28d325a38a2f1b257e71a
Kichirooo <andrew.reynard.h@gmail.com>
2026-05-22 14:49:10 +0700

    week 13: create save and load list functions

--- src/oop_00000110585_AndrewReynardHamdani/week13/StudentSerialization.kt
+++ src/oop_00000110585_AndrewReynardHamdani/week13/StudentSerialization.kt
@@ -6,7 +6,20 @@ data class Student(val name: String, val age: Int, val gpa: Double)
 
 fun Student.toCsv(): String = "$name, $age, $gpa"
 
-fun fromCsv(line String): Student {
+fun fromCsv(line: String): Student {
     val parts = line.split(",")
     return Student(parts[0], parts[1].toInt(), parts[2].toDouble())
+}
+
+fun saveStudents(students: List<Student>, path: String) {
+    File(path).writeText(students.joinToString("\n") { it.toCsv() })
+}
+
+fun loadStudents(path: String): List<Student> {
+    return try {
+        File(path).readLines().map { fromCsv(it) }
+    } catch (e:FileNotFoundException) {
+        println("Error: File tidek ditemukan")
+        emptyList()
+    }
 }
